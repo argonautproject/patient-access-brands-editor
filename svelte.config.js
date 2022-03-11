ed2aa2740e695b658fbb48bc76873586569008ec
Josh Mandel <jmandel@med.harvard.edu>
2022-03-11 11:03:02 -0600

    Check in built site

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +10,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
 	}
 };
 
